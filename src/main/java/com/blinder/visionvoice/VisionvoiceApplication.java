c6439a818f302bb0f8046ff6cd49ab66831448c3
Matheus Ramos de Pierro <matpierro570@gmail.com>
2024-07-24 22:24:10 -0300

    feat: create project with dependencies (first commit)

---
+++ src/main/java/com/blinder/visionvoice/VisionvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.blinder.visionvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blinder/visionvoice/VisionvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blinder.visionvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
